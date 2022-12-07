76672ccc0427efdba6d1ec3c1237afc24b18742c
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-07 19:06:46 +0100

    finished task 'first-react-app-with-jsx'

---
+++ react-intro/first-react-app-with-jsx/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="greeting">
+      <h1 className="greeting__title">Hello, world!</h1>
+      <p className="greeting__text">I'm learning React</p>
+    </div>
+  );
+}
+
+export default App;
