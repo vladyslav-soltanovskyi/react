7f2ac105df6b0276119bd62a79aee07a1e17aae2
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-08 20:01:09 +0100

    finished task 'greeting-component'

--- components-props/greeting-component/src/Greeting.jsx
+++ components-props/greeting-component/src/Greeting.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-function getCurrentAge(date) {
-  return ((new Date().getTime() - new Date(date)) / (24 * 3600 * 365.25 * 1000)) | 0;
+function getAge(date) {
+  var today = new Date();
+  var birthDate = new Date(date);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+  }
+  return age;
 }
 
 function Greeting({ firstName, lastName, birthDate }) {
