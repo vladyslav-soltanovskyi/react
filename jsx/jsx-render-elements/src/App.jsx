421b9f7a683f2bce4488988b373d466d6afc620e
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-08 17:39:29 +0100

    finished task 'jsx-render-elements'

---
+++ jsx/jsx-render-elements/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Search Form</h1>
+      <div>
+        <input type="text" />
+        <button>Search</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
