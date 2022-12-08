906844f2546d259d2c646ee9ac90944f5edf9b5d
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-08 18:19:52 +0100

    finished task 'todo-list'

---
+++ jsx/todo-list/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+    <h1 className="title">Todo List</h1>
+    <main className="todo-list">
+      <div className="actions">
+        <input className="task-input" type="text" />
+        <button className="btn create-task-btn">Create</button>
+      </div>
+      <ul className="list"></ul>
+    </main>
+    </>
+  );
+}
+
+export default App;
