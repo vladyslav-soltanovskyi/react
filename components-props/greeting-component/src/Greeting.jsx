c1567e8341c6a118a88690ff1a4a2ccf7b92a8a9
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-09 12:29:20 +0100

    finished task 'greeting-component'

--- components-props/greeting-component/src/Greeting.jsx
+++ components-props/greeting-component/src/Greeting.jsx
@@ -5,7 +5,7 @@ const getAge = (date) => moment().diff(new Date(date), "years");
 
 function Greeting({ firstName, lastName, birthDate }) {
   return (
-    <div className="greeting">My name is {firstName} {lastName}. I am {getAge(birthDate)} years old</div>
+    <div className="greeting">{`My name is ${firstName} ${lastName}. I am ${getAge(birthDate)} years old`}</div>
   );
 }
 
