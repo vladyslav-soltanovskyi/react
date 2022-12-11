9f4cd9283886d18b02ea223a38698dfbe2fedefe
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-11 16:08:22 +0100

    finished task 'users-list-with-key'

--- lists/users-list-with-key/src/UsersList.jsx
+++ lists/users-list-with-key/src/UsersList.jsx
@@ -5,31 +5,34 @@ class UsersList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: props.users,
-      sortText: '-'
+      sorting: ''
     }
   }
 
-  handleSortUsers = () => {
-    const { users, sortText } = this.state;
-    let copyUsers = [...users];
-    const isCurrentSortByDesc = sortText === '-' || sortText === 'desc';
-    
-    copyUsers = copyUsers.sort((a, b) => isCurrentSortByDesc ? (a.age - b.age) : (b.age - a.age));
+  handleChangeSort = () => {
+    const { sorting } = this.state;
+    const newSorting = sorting === 'asc' ? 'desc' : 'asc';
+
     this.setState({
-      users: copyUsers,
-      sortText: isCurrentSortByDesc ? 'asc' : 'desc'
+      sorting: newSorting
     })
   }
-
+  
   render() {
-    const { users, sortText } = this.state;
-
+    const { sorting } = this.state;
+    const { users } = this.props;
+    const isCurrentSortByDesc = sorting === 'desc';
+    let copyUsers = [...users];
+    
+    if (['asc', 'desc'].includes(sorting)) {
+      copyUsers = [...users].sort((a, b) => isCurrentSortByDesc ? (b.age - a.age) : (a.age - b.age));
+    }
+    
     return (
       <div>
-        <button class="btn" onClick={this.handleSortUsers}>{sortText}</button>
+        <button class="btn" onClick={this.handleChangeSort}>{sorting || '-'}</button>
         <ul class="users">
-          {users.map(user => <User key={user.name} {...user} />)}
+          {copyUsers.map(user => <User key={user.name} {...user} />)}
         </ul>
       </div>
     );
