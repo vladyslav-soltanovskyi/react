a44b2f36b31f63c926922005d2ea030ac9a986e9
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-11 18:08:27 +0100

    finished task 'transactions-list'

--- lists/transations-list/src/Transaction.jsx
+++ lists/transations-list/src/Transaction.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import moment from "moment";
 
 const formattingAmount = (amount) => new Intl.NumberFormat('en-GB').format(amount);
 
-const formattingTime = (date) => moment(date).locale('en-GB').format("DD MMM");
+const formattingTime = (date) => new Intl.DateTimeFormat('en-GB', {
+  minute: 'numeric',
+  hour: 'numeric'
+}).format(new Date(date));
 
-const formattingDate = (date) => moment(date).locale('en-GB').format("HH:mm");
+const formattingDate = (date) => new Intl.DateTimeFormat('en-GB', {
+  day: 'numeric',
+  month: 'short'
+}).format(new Date(date));
 
 function Transaction({ from, to, amount, rate, time }) {
 
   return (
     <li className="transaction">
-      <span className="transaction__date">{formattingTime(time)}</span>
-      <span className="transaction__time">{formattingDate(time)}</span>
+      <span className="transaction__date">{formattingDate(time)}</span>
+      <span className="transaction__time">{formattingTime(time)}</span>
       <span className="transaction__assets">{from} → {to}</span>
       <span className="transaction__rate">{rate}</span>
       <span className="transaction__amount">{formattingAmount(amount)}</span>
