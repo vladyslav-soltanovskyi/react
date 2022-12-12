3a0cf6173b3ec96bfe4889bd538af4dc0a83e3e5
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-12 15:31:48 +0100

    finished task 'life-component'

--- lifecycle-methods/life-component/src/Life.jsx
+++ lifecycle-methods/life-component/src/Life.jsx
@@ -4,14 +4,19 @@ class Life extends React.Component {
   constructor(props) {
     super(props);
     console.log("constructor: good place to create state");
+    this.state = {
+      count: 0
+    }
   }
 
   componentDidMount() {
+    this.timerId = setInterval(() => this.setState({ count: this.state.count + 1 }), 1000);
     console.log("componentDidMount: API calls, subscriptions");
   }
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log("shouldComponentUpdate(nextProps, nextState): decide to render or not to render");
+    return this.state.count > 0;
   }
   
   componentDidUpdate(prevProps, prevState) {
@@ -19,6 +24,7 @@ class Life extends React.Component {
   }
 
   componentWillUnmount() {
+    clearInterval(this.timerId);
     console.log("componentWillUnmount(): cleanup before DOM related to component will be removed");
   }
 
