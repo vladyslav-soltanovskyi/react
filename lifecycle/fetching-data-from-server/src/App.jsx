e17a6371991918a3379da67f521b6b94c429b18d
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-12 18:05:56 +0100

    finished task 'fetching-data-from-server'

---
+++ lifecycle/fetching-data-from-server/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import User from "./User";
+
+
+function App() {
+  return (
+    <User userId="gromcode" />
+  );
+}
+
+export default App;
---
+++ lifecycle/fetching-data-from-server/src/User.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+class Dimensions extends React.Component {
+  state = {
+    user: null
+  }
+  
+  fetchUserData = async (id) => {
+    const res = await fetch(`https://api.github.com/users/${id}`);
+    return await res.json();
+  }
+  
+  componentDidMount() {
+    this.fetchUserData(this.props.userId)
+      .then(user => {
+        this.setState({ user })
+      })
+  }
+  
+
+  render() {
+    const { user } = this.state;
+
+    if(!user) {
+      return null;
+    }
+
+    const { avatar_url, name, location } = user;
+    
+    return (
+      <div class="user">
+        <img
+          alt="User Avatar"
+          src={avatar_url}
+          class="user__avatar"
+        />
+        <div class="user__info">
+          <span class="user__name">{name}</span>
+          <span class="user__location">{location}</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dimensions;
