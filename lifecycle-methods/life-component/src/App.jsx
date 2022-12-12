18c37ef729a973d29310ecab25e75917b82748fb
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-12 15:13:56 +0100

    finished task 'life-component'

---
+++ lifecycle-methods/life-component/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Life from "./Life";
+
+
+function App() {
+  return (
+    <Life />
+  );
+}
+
+export default App;
---
+++ lifecycle-methods/life-component/src/Life.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+class Life extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor: good place to create state");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount: API calls, subscriptions");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate(nextProps, nextState): decide to render or not to render");
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate(prevProps, prevState): some updates based on new props");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount(): cleanup before DOM related to component will be removed");
+  }
+
+  render() {
+    console.log("return React element to build DOM");
+    return (
+      <div className="life">Life Component</div>
+    );
+  }
+}
+
+export default Life;
