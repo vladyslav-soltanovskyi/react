4e91db0ccd2120686f9e23c07cc83e48a570bffe
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-12 16:40:41 +0100

    finished task 'world-clock-2'

---
+++ lifecycle/world-clock-2/src/Clock.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const getTimeWithOffset = offset => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+};
+
+class Clock extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: getTimeWithOffset(props.offset)
+    }
+  }
+  
+  tick = () => this.setState({ date: getTimeWithOffset(this.props.offset) });
+
+  componentDidMount() {
+    this.timerId = setInterval(this.tick, this.props.interval);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    const { date } = this.state;
+    const { location } = this.props;
+    return (
+      <div className="clock">
+        <div className="clock__location">{location}</div>
+        <div className="clock__time">{date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true })}</div>
+      </div>
+
+    );
+  }
+}
+
+export default Clock;
